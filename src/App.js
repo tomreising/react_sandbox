e52a1038da48d164b87b9b857be9374bedf10771
Tom Reising <56168546+tomreising@users.noreply.github.com>
2023-07-03 18:46:03 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Genheader } from "./header";
 import "./styles.css";
+
 
 export default function App() {
   return (
     <div className="App">
+      < Genheader />
       <h1>Hello test one two three box</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
